4ae135e2ffd9bec2eff449a65ff3acd70bbdd7fa
fenggeHan <1198310863@qq.com>
2025-10-28 15:03:01 +0800

    Update granite-prediction.app.py

--- granite-prediction.app.py
+++ granite-prediction.app.py
@@ -3,7 +3,6 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-import joblib
 
 # 页面标题
 st.title("Granite Prediction App")
