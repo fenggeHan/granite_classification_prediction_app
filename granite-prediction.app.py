29efa32d6b8e19c72be25f2600cb8337bbccdabd
fenggeHan <1198310863@qq.com>
2025-11-06 11:03:48 +0800

    Update granite-prediction.app.py

--- granite-prediction.app.py
+++ granite-prediction.app.py
@@ -61,7 +61,7 @@ st.download_button(
     file_name="Data_Template-granite.csv",
     mime="text/csv"
 )
-st.info("Template download completed!")  # 模版下载完成提示
+st.success("Template download completed!")  # 模版下载完成提示
 # 步骤 2：用户上传数据
 st.subheader("Step 2: Please nUpload Your Data for Prediction")
 uploaded_file = st.file_uploader("Please upload a CSV file that matches the download template", type="csv")
@@ -106,6 +106,7 @@ if uploaded_file is not None:
         st.error(f"The uploaded CSV file should contain 25 feature columns. Please check the data format.")
 else:
     st.info("Please upload a CSV file that matches the template to make predictions.")
+
 
 
 
