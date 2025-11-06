327191c384e61f6b963b53be2af19de0ac54e270
fenggeHan <1198310863@qq.com>
2025-11-06 15:38:03 +0800

    Update granite-prediction.app.py

--- granite-prediction.app.py
+++ granite-prediction.app.py
@@ -128,9 +128,6 @@ st.subheader("Citation（引用）")
 st.write("* Han, F., Leng, C., Chen, J., Zou, S. & Wang, D. (2025). Machine lerarning method for discriminating granite genetic types based on trace element composition of apatite. Acta Petrologica Sinica, 41 (02), 737-750. (in Chinese with English abstract). doi: 10. 18654/1000-0569/")
 st.write("* 韩凤歌, 冷成彪, 陈加杰, 邹少浩,王大钊. 2025. 基于磷灰石微量元素组成的机器学习方法判别花岗岩成因类型. 岩石学报, 41(02): 737-750. doi: 10. 18654/1000-0569/")
 
-# 页面内容
-st.title("Gold Mineralization Prediction Model")
-st.write("Use this model to predict gold mineralization in rocks.")
 
 # 根据语言显示祝福语
 st.markdown(
@@ -142,6 +139,7 @@ st.markdown(
     </div>
     """, unsafe_allow_html=True
 )
+
 
 
 
