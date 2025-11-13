817a623283279d41951b71d272aea1c177902246
fenggeHan <1198310863@qq.com>
2025-11-13 11:23:37 +0800

    Update granite-prediction.app.py

--- granite-prediction.app.py
+++ granite-prediction.app.py
@@ -124,11 +124,11 @@ if uploaded_file is not None:
         st.markdown("""
             <style>
             div.stDownloadButton > button:first-child {
-                background-color: #FFF9B1;  /* 浅黄色 */
+                background-color: #FFFCDB;  /* 浅黄色 */
                 color: black;                /* 字体颜色 */
             }
             div.stDownloadButton > button:first-child:hover {
-                background-color: #FFFFA5;  /* 悬停时稍深的浅黄色 */
+                background-color: #FFF9B1;  /* 悬停时稍深的浅黄色 */
                 color: black;
             }
             </style>
@@ -175,6 +175,7 @@ st.markdown(
     <hr style="border: 0; border-top: 2px solid green; width: 100%; background-image: url('https://upload.wikimedia.org/wikipedia/commons/a/a5/Wave_pattern.svg'); height: 10px;">
     """, unsafe_allow_html=True
 )
+
 
 
 
